0a655d30c5985649233554952c97ad498645e81f
Bjoern Brandenburg <bbb@mpi-sws.org>
2017-03-07 06:43:32 +0100

    Clarify options, simplify JSON format

--- mktasks.py
+++ mktasks.py
@@ -25,7 +25,6 @@ def is_feasible(taskset):
     sol = apa_implicit_deadline_feasible(ts, aff)
     return True if sol else False
 
-
 def make_taskset(n, u, min_wcet=500):
     ts = emstada.gen_taskset('uni-broad', 'logunif', n, u,
             period_granularity=1, want_integral=True, scale=ms2us)
@@ -137,91 +136,110 @@ def to_hex(affinity):
     return hex
 
 def to_json(ts):
-    tsks = {}
-    for t in ts:
-        tsks[t.id] = {
+    tsks = []
+    for i, t in enumerate(ts):
+        tsks.append({
+            'id'       : i + 1,
             'cost'     : t.cost,
             'period'   : t.period,
             'affinity' : list(t.affinity),
             'priority' : t.priority,
-        }
+        })
     data = {
         'tasks' : tsks,
     }
     return json.dumps(data, sort_keys=True, indent=4, separators=(',', ': '))
 
-def store_random_taskset(m, n, u, seq, prefix=''):
-    print "[random laminar APAs, %d cores, %.2f utilization, and %d tasks per core]" \
-             % (m, u, n)
-    ts = make_taskset(n, u * m)
-    assign_random_laminar_affinities(ts, m)
-    assign_arm_priorities(ts)
-    fname = "%sapa-r-workload_m=%02d_n=%02d_u=%2d_seq=%02d.json" % \
-        (prefix, m, n, int(100 * u), seq)
+def store(ts, fname):
     print '=>', fname
     f  = open(fname, 'w')
     f.write(to_json(ts))
     f.close()
 
+def store_random_taskset(m, n, u, seq, prefix=''):
+    print "[random laminar APAs, %d cores, %.2f utilization, %d tasks]" \
+             % (m, u, n)
+    fname = "%sapa-r-workload_m=%02d_n=%02d_u=%2d_seq=%02d.json" % \
+        (prefix, m, n, int(100 * u), seq)
+    if exists(fname):
+        print '=> skipped; %s exists already.' % fname
+        return
+
+    ts = make_taskset(n, u * m)
+    assign_random_laminar_affinities(ts, m)
+    assign_arm_priorities(ts)
+
+    store(ts, fname)
+
 def store_partitioned_taskset(m, n, u, seq, prefix=''):
-    print "[partitioned, %d cores, %.2f utilization, and %d tasks per core]" \
-         % (m, u, n)
+    print "[pre-partitioned, %d cores, %.2f utilization, %.2f tasks per core]" \
+         % (m, u, n / m)
+    fname = "%spart-workload_m=%02d_n=%02d_u=%2d_seq=%02d.json" % \
+        (prefix, m, n, int(100 * u), seq)
+    if exists(fname):
+        print '=> skipped; %s exists already.' % fname
+        return
+
     ts = TaskSystem()
+    npc   = n // m
+    extra = n % m
     for core in xrange(m):
-        per_core = make_taskset(n, u)
+        per_core = make_taskset(npc + 1 if core < extra else npc, u)
         for t in per_core:
             t.partition = core
             t.affinity = set([core])
         ts += per_core
-
     assign_rm_priorities(ts)
-    fname = "%spart-workload_m=%02d_n=%02d_u=%2d_seq=%02d.json" % \
-        (prefix, m, n * m, int(100 * u), seq)
-    print '=>', fname
-    f  = open(fname, 'w')
-    f.write(to_json(ts))
-    f.close()
+
+    store(ts, fname)
 
 def store_socket_taskset(m, sockets, n, u, seq, prefix=''):
-    print "[socket-aware laminar APAs, %d cores, %d sockets, %.2f utilization, and %d tasks per core]" \
+    print "[socket-aware laminar APAs, %d cores, %d sockets, %.2f utilization, %d tasks]" \
          % (m, sockets, u, n)
     fname = "%sapa-s-workload_m=%02d_s=%02d_n=%02d_u=%2d_seq=%02d.json" % \
         (prefix, m, sockets, n, int(100 * u), seq)
     if exists(fname):
-        print 'Skipping %s; exists already.' % fname
+        print '=> skipped; %s exists already.' % fname
         return
+
     ts = make_taskset(n, u)
     assign_three_level_affinities(ts, m, sockets)
     assign_rm_priorities(ts)
-    fname = "%sapa-workload_m=%02d_n=%02d_u=%2d_seq=%02d.json" % \
-        (prefix, m, n, int(100 * u), seq)
-    print '=>', fname
-    f  = open(fname, 'w')
-    f.write(to_json(ts))
-    f.close()
 
+    store(ts, fname)
 
 def parse_args():
     p = argparse.ArgumentParser(
         description='LITMUS^RT workload generator')
+
+    def pos_int(s):
+        v = int(s)
+        if v <= 0:
+             raise argparse.ArgumentTypeError("must be positive")
+        return v
 
     p.add_argument(
         '--prefix', type=str, dest='prefix', default='',
         help='Prefix for the generated file[s]')
 
     p.add_argument(
-        '-m', '--num-cores', type=int, nargs='*', dest='ncores', default=[],
+        '-m', '--num-cores', type=pos_int, nargs='*', dest='ncores', default=[],
         help='processor counts to consider [multiple possible]')
     p.add_argument(
-        '-s', '--num-sockets', type=int, nargs='*', dest='nsockets', default=[1],
-        help='socket counts to consider [multiple possible]')
+        '-s', '--num-sockets', type=pos_int, nargs='*', dest='nsockets', default=[1],
+        help='socket counts to consider [multiple possible, default 1]')
     p.add_argument(
-        '-n', '--tasks-per-core', type=int, nargs='*', dest='ntasks', default=[5],
-        help='task counts to consider [multiple possible]')
+        '-n', '--num-tasks', type=pos_int, nargs='*', dest='ntasks',
+        default=[],
+        help='task counts to consider, absolute values [multiple possible]')
+    p.add_argument(
+        '-t', '--task-per-core', type=pos_int, nargs='*', dest='ntasks_per_core',
+        default=[],
+        help='task counts to consider, relative to -m [default 5]')
     p.add_argument(
         '-u', '--per-core-utilization', type=float, nargs='*', dest='utils',
         default=[0.5],
-        help='processor utilizations to consider [multiple possible]')
+        help='average processor utilizations to consider [multiple possible]')
 
     p.add_argument(
         '--apa', type=str, choices=['partitioned', 'random', 'socket'],
@@ -229,10 +247,22 @@ def parse_args():
         help='what sort of affinities to generate [default: partitioned]')
 
     p.add_argument(
-        '-c', '--count', type=int, dest='count', default=1,
+        '-c', '--count', type=pos_int, dest='count', default=1,
         help='how many task sets per #cores, #tasks, and util')
 
     return p.parse_args()
+
+def mktasks(m, u, n, apa_type='partitioned', nsockets=[1], seqno=0, prefix=''):
+    if apa_type == 'partitioned':
+        store_partitioned_taskset(m, n, u, seqno, prefix=prefix)
+    elif apa_type == 'random':
+        store_random_taskset(m, n, u, seqno, prefix=prefix)
+    elif apa_type == 'socket':
+        for s in nsockets:
+            if s <= m:
+                store_socket_taskset(m, s, n, u, seqno, prefix=prefix)
+    else:
+        assert False
 
 def main(args=sys.argv[1:]):
     options = parse_args()
@@ -242,21 +272,14 @@ def main(args=sys.argv[1:]):
         makedirs(prefix_dir)
 
     for m in options.ncores:
-        for n in options.ntasks:
-            for u in options.utils:
-                for seq in xrange(options.count):
-                    if options.apa_type == 'partitioned':
-                        store_partitioned_taskset(m, n, u, seq,
-                            prefix=options.prefix)
-                    elif options.apa_type == 'random':
-                        store_random_taskset(m, n, u, seq,
-                            prefix=options.prefix)
-                    elif options.apa_type == 'socket':
-                        for s in options.nsockets:
-                            store_socket_taskset(m, s, n, u, seq,
-                                prefix=options.prefix)
-                    else:
-                        assert False
+        for u in options.utils:
+            for seq in xrange(options.count):
+                for n in options.ntasks:
+                    mktasks(m, u, n,
+                        options.apa_type, options.nsockets, seq, options.prefix)
+                for t in options.ntasks_per_core:
+                    mktasks(m, u, t * m,
+                        options.apa_type, options.nsockets, seq, options.prefix)
 
 if __name__ == '__main__':
     main()
